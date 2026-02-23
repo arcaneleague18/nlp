94e136dfa71d4777653daf404c4b81a83044fdf8
arcaneleague18 <erenyeager4m@gmail.com>
2026-02-23 19:46:21 +0530

    updated

--- tokenization.py
+++ tokenization.py
@@ -1,6 +1,6 @@
 import re
 
-text = "Viswanath is an AI Engineer. He is a very smart intellectual. He loves working on complex problems and solving them."
+text = "Vis is an AI Engineer. He is a very smart intellectual. He loves working on complex problems and solving them."
 
 sentences = re.split(r'[.!?]+', text)
 sentences = [s.strip() for s in sentences if s.strip()]
